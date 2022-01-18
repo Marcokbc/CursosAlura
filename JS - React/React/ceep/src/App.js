8d11fd4de871bbf9370ecdb62f6c204b99ef3269
Marco Antônio Meira de Lima <mameira55@gmail.com>
2022-01-18 17:01:12 -0300

    react

---
+++ JS - React/React/ceep/src/App.js
@@ -0,0 +1,25 @@
+
+
+function App() {
+  return (
+    <section>
+      <form>
+        <input type="text" placeholder="Título"/>
+        <textarea placeholder="Escreva sua nota..." />
+        <button>Criar</button>
+      </form>
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Esreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+  </section>
+  );
+}
+
+export default App;
