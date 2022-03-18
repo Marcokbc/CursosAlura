971a226c041e4b1ff9169b66f31e6392c09e4b73
Marco <MAMeira55@gmail.com>
2022-03-17 22:47:56 -0300

    Atualização

--- JS - React/React/ceep/src/App.js
+++ JS - React/React/ceep/src/App.js
@@ -1,25 +1,35 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+  constructor(){
+    super();
 
+    this.state = {
+      notas:[]
+    }
+  }
+  
+  //Precisa disso aqui pois a lista de notas precisa saber de algo que o formulario fez
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
 
-function App() {
-  return (
-    <section>
-      <form>
-        <input type="text" placeholder="Título"/>
-        <textarea placeholder="Escreva sua nota..." />
-        <button>Criar</button>
-      </form>
-      <ul>
-        <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-            </header>
-            <p>Esreva sua nota</p>
-          </section>
-        </li>
-      </ul>
-  </section>
-  );
+  render() {
+    return (
+      <section className="conteudo">
+         <FormularioCadastro criarNota={this.criarNota.bind(this)}/> { /*  Quando for criarNota propriedade, chama o this.criarNota */}
+        <ListaDeNotas notas={this.state.notas}/>
+      </section>
+    );
+  }
 }
-
+//new ListaDeNotas({notas:this.notas})
 export default App;
