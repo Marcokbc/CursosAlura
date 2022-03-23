0add47eb678ac931eb487a312e54f4a50442694b
Marco <MAMeira55@gmail.com>
2022-03-23 18:32:32 -0300

    Conclusão de mais um curso na Alura

--- JS - React/React/ceep/src/components/CardNota/CardNota.jsx
+++ JS - React/React/ceep/src/components/CardNota/CardNota.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./estilo.css";
-import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg";
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg"
 class CardNota extends Component {
+
   apagar(){
     const indice = this.props.indice;
     this.props.apagarNota(indice);
   }
+  
   render() {
     return (
       <section className="card-nota">
@@ -15,6 +17,7 @@ class CardNota extends Component {
           <h4>{this.props.categoria}</h4>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
+
       </section>
     );
   }
